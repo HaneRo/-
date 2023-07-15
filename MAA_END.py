3a7865a6d02e0715bb6b90fc9b43b9a301e6b302
HaneRo <piumarugiada@hotmail.com>
2023-07-15 23:18:25 +0800

    Update MAA_END.py
    修复选择剩余理智后只统计剩余理智掉落

--- MAA_END.py
+++ MAA_END.py
@@ -2,7 +2,7 @@
 import telebot
 from datetime import datetime, timedelta
 import re
-bot = telebot.TeleBot("XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXX")
+bot = telebot.TeleBot("5970225649:AAGJaSAkPfGyN3VGMkld7FshVAHmMyxcm0w")
 
 def remove_brackets(input_string):
     output_string = re.sub(r'\([^)]*\)', '', input_string)
@@ -11,7 +11,8 @@ def remove_brackets(input_string):
 try:
     atk_count = 0
     msg=""
-
+    getlist=""
+    counts = {}
     with open('./debug/gui.log', encoding='utf-8') as log:
         should_read = False  # 是否开始读取标志
         lines_to_read = []  # 存储需要读取的行
@@ -19,11 +20,22 @@ try:
         contents = log.readlines()
         for line in contents:
             line= remove_brackets(line)
-            if "开始任务: Fight" in line:
+            if "开始任务: 开始前脚本" in line:
                 atk_count = 0
                 msg=""
+                getlist=""
+                lines_to_read = [] 
+                counts = {}
                 start_time=datetime.strptime(line[:25], '[%Y-%m-%d %H:%M:%S.%f]')
-                # print(start_time)
+            # if "开始任务: Fight" in line:
+            #     print("开始任务")
+            if "完成任务: Fight" in line:
+                for item in lines_to_read:
+                    key, value = item.split(':')
+                    if key in counts:
+                        counts[key] += int(value)
+                    else:
+                        counts[key] = int(value)
             if "已开始行动" in line:
                 atk_count += 1
             if "任务出错" in line:
@@ -39,11 +51,15 @@ try:
                 # 检测到起始关键字，开始读取
                 should_read = True
                 lines_to_read = [] 
+                
+    result = [f'{key}:{value}' for key, value in counts.items()]
+    print(counts)
+    getlist+='、'.join(result)
+    if len(getlist) != 0:
+        msg+=",共获取到"
+    msg+=getlist
 
-    msg+=",共获取到"
-    msg+='、'.join(lines_to_read)
     diff = datetime.now() - start_time
-
     # 定义时间阈值为2小时
     threshold = timedelta(hours=2)
     # 比较时间差是否小于阈值
@@ -52,7 +68,7 @@ try:
     else:
         print("时间差大于等于2小时")
         msg+="\n！！！注意开始时间与当前时间相差2小时以上，请检查程序是否正常运行！！！"
-    bot.send_message(chat_id='XXXXXXX',
+    bot.send_message(chat_id='1439363098',
                      text="MAA操作完成，共行动"+str(atk_count)+"次"+str(msg))
     print("MAA操作完成，共行动"+str(atk_count)+"次"+str(msg))
 except:
